d788ba6ab439a6e68f4d19eaa2e4f447411f8d7c
andrii-2020 <bebion58@gmail.com>
2025-02-17 15:37:45 +0100

    Ser

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,19 +1,19 @@
 export class Services {
     getAllCategories() {
-        return  fetch(`/apicategories/`).then(value => value.json());
+        return  fetch(`/api/api/categories/`).then(value => value.json());
     }
 
     
     getAllProducts() {
-        return fetch(`/api/products/`).then(value => value.json())
+        return fetch(`/api/api/products/`).then(value => value.json())
     }
 
     getProductsLists(categoryId) {
-        return fetch(`/api/products/?category=${categoryId}`).then(value => value.json())
+        return fetch(`/api/api/products/?category=${categoryId}`).then(value => value.json())
     }
 
     getProductID(id) {
-        return fetch(`/api/products/${id}/`).then(value => value.json())
+        return fetch(`/api/api/products/${id}/`).then(value => value.json())
     }
 
      
