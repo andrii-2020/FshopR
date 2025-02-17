c3eb200430dc50fa331b5a4e856d8627b65c0796
andrii-2020 <bebion58@gmail.com>
2025-02-17 15:45:16 +0100

    btn_color

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,19 +1,19 @@
 export class Services {
     getAllCategories() {
-        return  fetch(`/apim/api/categories/`).then(value => value.json());
+        return  fetch(`${process.env.REACT_APP_API_URL}/categories/`).then(value => value.json());
     }
 
     
     getAllProducts() {
-        return fetch(`/apim/api/products/`).then(value => value.json())
+        return fetch(`${process.env.REACT_APP_API_URL}/products/`).then(value => value.json())
     }
 
     getProductsLists(categoryId) {
-        return fetch(`/apim/api/products/?category=${categoryId}`).then(value => value.json())
+        return fetch(`${process.env.REACT_APP_API_URL}/products/?category=${categoryId}`).then(value => value.json())
     }
 
     getProductID(id) {
-        return fetch(`/apim/api/products/${id}/`).then(value => value.json())
+        return fetch(`${process.env.REACT_APP_API_URL}/products/${id}/`).then(value => value.json())
     }
 
      
