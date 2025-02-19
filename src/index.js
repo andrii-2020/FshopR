d451ddb844ab1e3c36c36d714ecaf54d4939fcb1
andrii-2020 <bebion58@gmail.com>
2025-02-19 14:30:36 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NoMatch from './page/NoMatch';
 import ProductList from './page/ProductList';
