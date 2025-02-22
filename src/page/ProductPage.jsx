9d975cacc9849b875e6d28a7e1f0d824781f47d1
andrii-2020 <bebion58@gmail.com>
2025-02-22 20:47:40 +0100

    app ne3

--- src/page/ProductPage.jsx
+++ src/page/ProductPage.jsx
@@ -24,7 +24,8 @@ const ProductPage = () => {
   useEffect(() => {
     if (id) {
       _Services.getProductID(id).then(res => {
-        setProduct(res)
+
+        setProduct(res[0])
       }).catch(error => {
         console.warn(error)
       })
@@ -44,59 +45,72 @@ const ProductPage = () => {
 
 
   return (
-    <div className="max-w-6xl mx-auto">
-       
-      <div style={{ textAlign: 'center', width: '180px', display: 'flex', position: 'absolute', zIndex: 99999, padding: '18px' }}>
-        <code style={{ paddingTop: 7, paddingRight: 10 }}>Go Home</code>
-        <HomeIcon
-          size={40}
-          onClick={() => {
-            navigate('/');
-          }}
-        />
-      </div>
-      <CardP className="grid md:grid-cols-2 gap-8 p-6">
-        <div className="mySlides fade">
-          <ProductImage
-            imageUrl={product.images}
-          />
-        </div>
+    <>
+        {product &&
+            <div className="max-w-6xl mx-auto">
+
+                <div style={{
+                    textAlign: 'center',
+                    width: '180px',
+                    display: 'flex',
+                    position: 'absolute',
+                    zIndex: 99999,
+                    padding: '18px'
+                }}>
+                    <code style={{paddingTop: 7, paddingRight: 10}}>Go Home</code>
+                    <HomeIcon
+                        size={40}
+                        onClick={() => {
+                            navigate('/');
+                        }}
+                    />
+                </div>
+                <CardP className="grid md:grid-cols-2 gap-8 p-6">
+                    <div className="mySlides fade">
+                        <ProductImage
+                            imageUrl={product.images}
+                        />
+                    </div>
 
 
-        <ProductDetails
-          {...product}
-          selectedSize={product.sizes}
-          selectedColor={product.colors}
-          onSizeChange={onSizeChanges}
-          onColorChange={onColorChanges}
-        />
-        {seletColor.length >= 0 && seletSize.length >= 0 ?
-        <p style={{ textAlign: 'center', marginBottom: 10, color: 'red' }}>
-          Вибери Розмір:  
-          <span style={{ color: 'black' }}>{ seletSize ? ' ' + seletSize + ' '  : ' ??? '}</span> 
-           і Колір:  <span style={{ color: 'black' }}>{ seletColor ? seletColor:' ??? '}</span>
-        </p>: <>dwdwdwd</>}
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          
-          <Button
-            btnD={seletColor.length > 0 && seletSize.length > 0 ? false : true}
-            variant='outline'
-            style={{ display: 'flex', background: 'red', color: 'white' }}
-            size='sm'
-            onClick={() => { _goForm('/form', {
-              state: {
-                color:seletColor, 
-                size: seletSize,
-                name: product.name,
-                oldPrice: product.price,
-                price: Number(product.newPrice) > 0? product.newPrice: product.price,
-              }
-            }) }}
-          />
-        </div>
-      </CardP>
-         
-    </div>
+                    <ProductDetails
+                        {...product}
+                        selectedSize={product.sizes}
+                        selectedColor={product.colors}
+                        onSizeChange={onSizeChanges}
+                        onColorChange={onColorChanges}
+                    />
+                    {seletColor.length >= 0 && seletSize.length >= 0 ?
+                        <p style={{textAlign: 'center', marginBottom: 10, color: 'red'}}>
+                            Вибери Розмір:
+                            <span style={{color: 'black'}}>{seletSize ? ' ' + seletSize + ' ' : ' ??? '}</span>
+                            і Колір: <span style={{color: 'black'}}>{seletColor ? seletColor : ' ??? '}</span>
+                        </p> : <>dwdwdwd</>}
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+
+                        <Button
+                            btnD={seletColor.length > 0 && seletSize.length > 0 ? false : true}
+                            variant='outline'
+                            style={{display: 'flex', background: 'red', color: 'white'}}
+                            size='sm'
+                            onClick={() => {
+                                _goForm('/form', {
+                                    state: {
+                                        color: seletColor,
+                                        size: seletSize,
+                                        name: product.name,
+                                        oldPrice: product.price,
+                                        price: Number(product.newPrice) > 0 ? product.newPrice : product.price,
+                                    }
+                                })
+                            }}
+                        />
+                    </div>
+                </CardP>
+
+            </div>
+        }
+    </>
   );
 };
 
