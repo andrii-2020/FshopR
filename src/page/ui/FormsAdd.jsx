ae3b780ae8412422db1c122b3921a276cf3d22e5
andrii-2020 <bebion58@gmail.com>
2025-02-25 19:09:07 +0100

    update form

--- src/page/ui/FormsAdd.jsx
+++ src/page/ui/FormsAdd.jsx
@@ -41,6 +41,7 @@ export default function FormsAdd() {
     const _goHome = useNavigate()
     const SendMessage = () => {
         console.log(bookingData)
+        setShowAlert(true)
         fetch(`${config.apiUrl}/sms/`, {
             method: 'POST',
             headers: {
@@ -55,7 +56,7 @@ export default function FormsAdd() {
         setTimeout(()=>{
             setShowAlert(false)
             _goHome('/')
-        }, 6000)
+        }, 5000)
 
     };
 
@@ -69,7 +70,9 @@ export default function FormsAdd() {
 
                     {showAlert && 
                         <div className="bg-blue-100 border-t border-b border-blue-500
-                        text-blue-700 px-4 py-3 p-5 text-center animate-bounce"  role="alert">
+                        text-blue-700 px-4 py-3 p-5 text-center animate-bounce"  role="alert"
+                            style={{top: '50%', position: 'sticky'}}
+                        >
                         <p className="font-bold">Дякуємо за замовлення.</p>
                         <p className="text-sm">На протязі 1-год. з вами зв'яжиться продавець</p>
                     </div>
@@ -134,7 +137,7 @@ export default function FormsAdd() {
                                     <label
                                         style={{color: validPhone === false ? 'red' : 'white'}}
                                         className="block mb-2 text-sm font-medium text-gray-900">
-                                        {validPhone? `....` : 'Номер телефону: 0682223344'}
+                                        {validPhone? `${bookingData.phone}` : 'Номер телефону: 0682223344'}
                                     </label>
                                     <input
                                         placeholder='0682223344'
